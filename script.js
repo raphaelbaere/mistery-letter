f17a11e283c60e37bf8430f92440c7148784436d
raphaelbaere <raphaelbaere@id.uff.br>
2022-09-05 20:23:51 -0300

    Só falta um requisito!

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const input = document.querySelector('#carta-texto');
 const buttonCreateCarta = document.querySelector('#criar-carta');
 const cartaGerada = document.querySelector('#carta-gerada');
+const classes = ['newspaper', 'magazine1', 'magazine2', 'medium', 'big', 'reallybig',
+  'rotateleft', 'rotateright', 'skewleft', 'skewright'];
+const contador = document.querySelector('#carta-contador');
 
 buttonCreateCarta.addEventListener('click', () => {
   if (!input.value.trim()) {
     cartaGerada.innerHTML = 'Por favor, digite o conteúdo da carta.';
   } else {
-  cartaGerada.innerHTML = '';
-  const cartaSeparada = input.value.split(' ');
-  for (let index = 0; index < cartaSeparada.length; index += 1) {
-    const createSpan = document.createElement('span');
-    createSpan.innerHTML = cartaSeparada[index];
-    cartaGerada.appendChild(createSpan);
+    cartaGerada.innerHTML = '';
+    const cartaSeparada = input.value.split(' ');
+    for (let index = 0; index < cartaSeparada.length; index += 1) {
+      const createSpan = document.createElement('span');
+      createSpan.innerHTML = cartaSeparada[index];
+      createSpan.className = `${classes[Math.floor(Math.random() * classes.length)]
+      } ${classes[Math.floor(Math.random() * classes.length)]} ${
+        classes[Math.floor(Math.random() * classes.length)]} ${
+        classes[Math.floor(Math.random() * classes.length)]}`;
+      createSpan.addEventListener('click', (event) => {
+        event.target.className = `${classes[Math.floor(Math.random() * classes.length)]} ${classes[Math.floor(Math.random() * classes.length)]} ${classes[Math.floor(Math.random() * classes.length)]} ${classes[Math.floor(Math.random() * classes.length)]}`;
+      });
+      cartaGerada.appendChild(createSpan);
+    }
+    contador.innerHTML = cartaGerada.childElementCount;
   }
- }
 });
